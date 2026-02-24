74057e655453238090a951cef6e6872d657e02f5
Umidjonjuraqulov <juraqulovumidjon0210@gmail.com>
2026-02-24 12:27:51 +0500

    Reset: upload fresh project

--- menus/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.conf import settings
-# Create your views here.
-
-def menu_view(request):
-    return render(request, 'home.html', {
-        'media_url': settings.MEDIA_URL
-    })
--- pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('education/', views.education_view, name='education'),
-    path('certificates/', views.certifocates_view, name='certificates'),
-    path('work/', views.work_view, name='work'),
-]
